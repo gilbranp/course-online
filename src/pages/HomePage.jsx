ad39072b3190e83eef9a55e631d25ef7fc4bec2a
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-18 09:47:20 +0700

    homePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ export default function HomePage() {
   return (
     <div className="home">
       <header className="w-100 min-vh-100"></header>
+      <div className="kelas w-100 min-vh-100"></div>
     </div>
   )
 }
