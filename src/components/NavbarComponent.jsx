8a10eb9b0d597ea8aefc1c14d1b284dc85319429
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-18 07:06:19 +0700

    nav

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -1,3 +1,4 @@
+import {useState,useEffect} from "react";
 import {Navbar,Container,Nav} from "react-bootstrap";
 import {navLinks} from "../data/index";
 import { NavLink } from "react-router-dom";
@@ -5,22 +6,24 @@ import { NavLink } from "react-router-dom";
 export const NavbarComponent = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="#home">GilbranID.</Navbar.Brand>
+        <Navbar.Brand href="#home" className="fs-3 fw-bold">GilbranID.</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mx-auto">
+          <Nav className="mx-auto text-center">
           {navLinks.map((link) => {
             return (
               <div className="nav-link" key={link.id}>
-                <NavLink to={link.path}>{link.text}</NavLink>
+                <NavLink to={link.path} className={({ isActive, isPending }) =>
+                (isPending ? "pending" : isActive ? "active" : "" )} end>
+                 {link.text}</NavLink>
             </div>);
           })}
           </Nav>
 
-          <div>
-            <button>Join With Us</button>
+          <div className="text-center">
+            <button className="btn btn-outline-danger rounded-1">Join With Us</button>
           </div>
         </Navbar.Collapse>
       </Container>
