0e96e57e1fe34622d8236dbe3958c1763ac15512
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-18 10:00:02 +0700

    homeePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,22 @@
+import {Container, Row, Col} from "react-bootstrap";
+import HeroImage from '../assets/img/hero.png'
+
 export default function HomePage() {
   return (
     <div className="home">
-      <header className="w-100 min-vh-100"></header>
+      <header className="w-100 min-vh-100">
+        <Container>
+          <Row>
+            <Col>
+              <h1>
+              Temukan <br/><span>Bakat Kreatifmu</span><br/>  Bersama Kami !
+              </h1>
+              <p>l</p>
+            </Col>
+            <Col></Col>
+          </Row>
+        </Container>
+      </header>
       <div className="kelas w-100 min-vh-100"></div>
     </div>
   )
