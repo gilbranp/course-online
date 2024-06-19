3dccc2d98f6bc8d67c46be4a8950874cced9b3d9
hosting.gilbranid <hosting.gilbranid@gmail.com>
2024-06-19 02:37:33 +0000

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,14 @@ import {Container, Row, Col} from "react-bootstrap";
 import HeroImage from '../assets/img/hero.png'
 
 import {kelasTerbaru} from '../data/index';
+import { useNavigate } from "react-router-dom"
+
+
 
 export default function HomePage() {
+
+  let navigate = useNavigate();
+
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
@@ -34,17 +40,28 @@ export default function HomePage() {
           <Row>
             {kelasTerbaru.map((kelas) => {
               return <Col key={kelas.id}>
-                <img src={kelas.image} alt="unsplah.com" />
-                <div>
+                <img src={kelas.image} alt="unsplah.com" className="w-100 mb-5 rounded-top" />
+                <div className="star mb-2 px-3">
                   <i className={kelas.star1}></i>
                   <i className={kelas.star2}></i>
                   <i className={kelas.star3}></i>
                   <i className={kelas.star4}></i>
                   <i className={kelas.star5}></i>
                 </div>
+                <h5 className="mb-5 px-3">{kelas.title}</h5>
+                <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                  <p className="mb-0 text-primary fw-bold">{kelas.price}</p>
+                  <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                </div>
               </Col>;
             })}
             <Col>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="text-center">
+              <button className="btn btn-success rounded-5 btn-lg" onClick={()=> navigate("/kelas")}>Lihat Semua Kelas<i className="fa-solid fa-chevron-right ms-1"></i></button>
+              
             </Col>
           </Row>
         </Container>
