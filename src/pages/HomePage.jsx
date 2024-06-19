8149a975cc84b6787799198cfb8104ad3fe7e3f4
hosting.gilbranid <hosting.gilbranid@gmail.com>
2024-06-19 14:04:18 +0000

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import {Container, Row, Col} from "react-bootstrap";
 import HeroImage from '../assets/img/hero.png'
 
-import {kelasTerbaru} from '../data/index';
+import {kelasTerbaru, dataSwiper} from '../data/index';
 import { useNavigate } from "react-router-dom"
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 
 export default function HomePage() {
@@ -14,7 +22,7 @@ export default function HomePage() {
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
-          <Row className="header-box d-flex align-items-center">
+          <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6">
               <h1 className="mb-4">
               Temukan <br/><span>Bakat Kreatifmu</span><br/>  Bersama Kami !
@@ -39,7 +47,7 @@ export default function HomePage() {
           </Row>
           <Row>
             {kelasTerbaru.map((kelas) => {
-              return <Col key={kelas.id}>
+              return <Col key={kelas.id} className="shadow rounded">
                 <img src={kelas.image} alt="unsplah.com" className="w-100 mb-5 rounded-top" />
                 <div className="star mb-2 px-3">
                   <i className={kelas.star1}></i>
@@ -63,6 +71,58 @@ export default function HomePage() {
               <button className="btn btn-success rounded-5 btn-lg" onClick={()=> navigate("/kelas")}>Lihat Semua Kelas<i className="fa-solid fa-chevron-right ms-1"></i></button>
               
             </Col>
+          </Row>
+        </Container>
+      </div>
+      <div className="testimonial py-5">
+        <Container>
+          <Row>
+            <Col>
+            <h1 className="text-center fw-bold my-5">Testimonial</h1>
+            </Col>
+          </Row>
+          <Row>
+             <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          992: {
+            slidesPerView: 2,
+            spaceBetween: 50,
+          },
+          1200: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+          },
+          
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {dataSwiper.map((data) => {
+          return <SwiperSlide key={data.id} className="shadow-sm">
+            <p className="desc">{data.desc}</p>
+            <div className="people">
+              <img src={data.image} alt="" />
+              <div>
+                <h5 className="mb-1">{data.name}</h5>
+                <p className="m-0 fw-bold">{data.skill}</p>
+              </div>
+            </div>
+          </SwiperSlide>;
+        })}
+      </Swiper>
           </Row>
         </Container>
       </div>
