36a5f46b1270dd70f71204904846b93046d61c36
LAPTOP-E2U5TP4H\ASUS <gilbranbambimboo@gmail.com>
2024-06-20 10:11:38 +0700

    upload for vsc

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,5 +1,33 @@
+import {Container, Row, Col} from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 export default function FooterComponent() {
   return (
-    <div>FooterComponent</div>
+    <div className="footer py-5">
+      <Container>
+       <Row>
+        <Col>
+         <h3 className="fw-bold">GilbranID</h3>
+         <p className="desc">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio nemo ad voluptas. Quisquam, odit architecto! Id dolore ipsam atque molestiae.</p>
+        <div className="no mb-1 mt-4">
+          <Link className="text-decoration-none">
+          <i className="fa-brands fa-whatsapp"></i>
+          <p className="m-0">+6281268477296</p>
+          </Link>
+        </div>
+        <div className="mail">
+          <Link className="text-decoration-none">
+          <i className="fa-regular fa-envelope"></i>
+          <p className="m-0">gilbranbambimboo@gmail.com</p>
+          </Link>
+        </div>
+        </Col>
+        <Col>
+         <h5></h5>
+        </Col>
+        <Col></Col>
+       </Row>
+      </Container>
+      </div>
   )
 }
